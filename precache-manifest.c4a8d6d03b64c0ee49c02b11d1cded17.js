16046e901253b1b7c1ac9832e674cf6028b87a0a
Yuds16 <yudhis.onggowarsito@gmail.com>
2020-08-30 19:03:06 +0700

    Updates

---
+++ precache-manifest.c4a8d6d03b64c0ee49c02b11d1cded17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d44c2f77d24184c9c7bc821de0d7699a",
+    "url": "/reactjs-edit-googlesheets/index.html"
+  },
+  {
+    "revision": "7fb6e4a657c2f843ca9e",
+    "url": "/reactjs-edit-googlesheets/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "3dce2758d14c02388a40",
+    "url": "/reactjs-edit-googlesheets/static/js/2.4354ce25.chunk.js"
+  },
+  {
+    "revision": "44b724887c788dd91af1f626e24324fa",
+    "url": "/reactjs-edit-googlesheets/static/js/2.4354ce25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb6e4a657c2f843ca9e",
+    "url": "/reactjs-edit-googlesheets/static/js/main.ef52e550.chunk.js"
+  },
+  {
+    "revision": "b6061674eb0009832332",
+    "url": "/reactjs-edit-googlesheets/static/js/runtime-main.426d963a.js"
+  }
+]);
